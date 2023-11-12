2ff6be95b5164e5f1624c102bf67b434f092b8dd
Javier Serna <hipc04.javier@gmail.com>
2023-11-12 02:25:34 -0800

    Update and rename runner.sh to runner_B.sh

--- runner.sh
+++
@@ -1,4 +0,0 @@
-python model_results.py
-python model_increase.py
-python model_constant_1.py
-python model_constant_2.py
---
+++ runner_B.sh
@@ -0,0 +1,4 @@
+python model_results_B.py
+python model_increase_B.py
+python model_constant_B_1.py
+python model_constant_B_2.py
