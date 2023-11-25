e3dc2bca5c878104512f250cad2b9593b794aea9
Javier Serna <hipc04.javier@gmail.com>
2023-11-24 19:49:02 -0800

    Add files via upload

---
+++ Back-Forward-Tests/runner.sh
@@ -0,0 +1,8 @@
+python model_results_B.py
+python model_increase_B.py
+python model_constant_B_1.py
+python model_constant_B_2.py
+python model_results_chi.py
+python model_increase_chi.py
+python model_constant_chi_1.py
+python model_constant_chi_2.py
